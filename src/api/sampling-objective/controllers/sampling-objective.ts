9ac16bad471d56ffab666e635ed5c25961d3f08e
ckchinmay <ckchinmaykulkarni@gmail.com>
2023-09-04 09:49:37 +0200

    Updated relations, optimized import operation.

---
+++ src/api/sampling-objective/controllers/sampling-objective.ts
@@ -0,0 +1,7 @@
+/**
+ * sampling-objective controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::sampling-objective.sampling-objective');
---
+++ src/api/sampling-objective/services/sampling-objective.ts
@@ -0,0 +1,7 @@
+/**
+ * sampling-objective service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sampling-objective.sampling-objective');
