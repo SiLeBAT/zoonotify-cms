e0c25257e71d396a870b1d3e023c86fcd00854d7
Dominic Tölle <dominic.tolle@gmail.com>
2023-11-10 10:18:21 +0100

    Add Evaluation Information Content-type
    	- Fixed welcome content-type capitalization error
    
    Ticket: client#640

---
+++ src/api/evaluation-information/controllers/evaluation-information.ts
@@ -0,0 +1,7 @@
+/**
+ * evaluation-information controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::evaluation-information.evaluation-information');
---
+++ src/api/evaluation-information/routes/evaluation-information.ts
@@ -0,0 +1,7 @@
+/**
+ * evaluation-information router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::evaluation-information.evaluation-information');
