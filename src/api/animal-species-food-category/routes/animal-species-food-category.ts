721740396e8c3b86c85ed07734371ce38d8a9d6e
ckchinmay <ckchinmaykulkarni@gmail.com>
2023-11-29 15:48:27 +0100

    Updated content structure for showing LD UI

--- src/api/animal-species-food-category/routes/animal-species-food-category.ts
+++
@@ -1,7 +0,0 @@
-/**
- * animal-species-production-direction-food router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::animal-species-production-direction-food.animal-species-production-direction-food');
---
+++ src/api/animal-species-food-upper-category/services/animal-species-food-upper-category.ts
@@ -0,0 +1,7 @@
+/**
+ * animal-species-food-upper-category service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::animal-species-food-upper-category.animal-species-food-upper-category');
