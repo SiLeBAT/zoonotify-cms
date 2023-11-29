a166e9d6d98930e8e264ff84ddc00f3650c7f829
ckchinmay <ckchinmaykulkarni@gmail.com>
2023-11-29 15:48:27 +0100

    Updated content structure for showing LD UI

---
+++ src/api/animal-species-food-upper-category/routes/animal-species-food-upper-category.ts
@@ -0,0 +1,7 @@
+/**
+ * animal-species-food-upper-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::animal-species-food-upper-category.animal-species-food-upper-category');
