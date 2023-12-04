43299ba8b51300dc219833d70634a9c422f2c5b8
ckchinmay <ckchinmaykulkarni@gmail.com>
2023-12-04 12:57:34 +0100

    Changed "upper" to "top" in all models

--- src/api/animal-species-food-upper-category/controllers/animal-species-food-upper-category.ts
+++
@@ -1,7 +0,0 @@
-/**
- * animal-species-food-upper-category controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::animal-species-food-upper-category.animal-species-food-upper-category');
--- src/api/animal-species-food-upper-category/services/animal-species-food-upper-category.ts
+++
@@ -1,7 +0,0 @@
-/**
- * animal-species-food-upper-category service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::animal-species-food-upper-category.animal-species-food-upper-category');
