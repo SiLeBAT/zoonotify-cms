815a28ce154e6bc4179f8753dc93b346e9807893
ckchinmay <ckchinmaykulkarni@gmail.com>
2023-12-06 10:46:39 +0100

    Added fix for regenerating types , way around bug in strapi

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -23,6 +23,7 @@ export interface AdminPermission extends Schema.CollectionType {
       Attribute.SetMinMaxLength<{
         minLength: 1;
       }>;
+    actionParameters: Attribute.JSON & Attribute.DefaultTo<{}>;
     subject: Attribute.String &
       Attribute.SetMinMaxLength<{
         minLength: 1;
@@ -1663,7 +1664,7 @@ export interface ApiWelcomeWelcome extends Schema.SingleType {
   };
 }
 
-declare module '@strapi/strapi' {
+declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
       'admin::permission': AdminPermission;
