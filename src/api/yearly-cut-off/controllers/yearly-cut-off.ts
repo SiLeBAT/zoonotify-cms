01fa62d2af4a34f5074a611ded6267a2a11642b7
ckchinmay <ckchinmaykulkarni@gmail.com>
2023-12-29 08:06:46 +0100

    New collections for yearly cutoff data

---
+++ src/api/yearly-cut-off/controllers/yearly-cut-off.ts
@@ -0,0 +1,7 @@
+/**
+ * yearly-cut-off controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::yearly-cut-off.yearly-cut-off');
---
+++ src/api/yearly-cut-off/routes/yearly-cut-off.ts
@@ -0,0 +1,7 @@
+/**
+ * yearly-cut-off router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::yearly-cut-off.yearly-cut-off');
