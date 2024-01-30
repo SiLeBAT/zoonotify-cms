bae230918ae83843ac0b5f27d9f12f00e623a17a
Dominic Tölle <dominic.tolle@gmail.com>
2024-01-30 09:39:46 +0100

    Replace Animal Species Production Direction Food CT with Animal species Production type/Food
    Ticket: #110

---
+++ src/api/animal-species-production-type-food/controllers/animal-species-production-type-food.ts
@@ -0,0 +1,7 @@
+/**
+ * animal-species-production-type-food controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::animal-species-production-type-food.animal-species-production-type-food');
