68bccdea089fd3104d932a366135c3f08dfe51c8
mahtabiltarabian <m.iltarabian68@gmail.com>
2024-03-20 15:42:11 +0100

    Controlled Vocabulary translation imported into the Controlled Vocabulary Collection Type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import xlsx from 'node-xlsx';
+
 let antibiotics;
 export default {
   /**
@@ -25,6 +26,9 @@ export default {
 
     // Import data for prevalence 
     await importPrevalenceData(strapi);
+
+    // Add this line to call your new import function
+    await importControlledVocabularyTranslations(strapi);
 
   }
 
@@ -339,4 +343,45 @@ export interface CutOff {
   min: number;
   max: number;
   cutOff: string;
+}
+async function importControlledVocabularyTranslations(strapi) {
+ const fs = require('fs');
+ const path = require('path');
+  
+  let filePath = path.join(__dirname, '../../data//master-data/ZooNotify_DB_translation (1).xlsx');
+
+  if (fs.existsSync(filePath)) {
+    const buffer = fs.readFileSync(filePath);
+    const dataFromExcel = xlsx.parse(buffer); // Parse the Excel file
+    const translationData = dataFromExcel.find(sheet => sheet.name === 'translation'); // Find the 'translation' sheet
+
+    // Assuming your Controlled Vocabulary collection type is correctly defined in Strapi as outlined before
+    let dataList = translationData.data.slice(1).map(row => { // Skip header row
+      return {
+        de: row[1], // German translation
+        en: row[2]  // English translation
+      };
+    });
+
+    for (const item of dataList) {
+      try {
+        // Check if the entry already exists
+        let existingEntries = await strapi.entityService.findMany('api::controlled-vocabulary.controlled-vocabulary', {
+          filters: { $or: [{ de: item.de }, { en: item.en }] },
+        });
+
+        if (existingEntries.length > 0) {
+          // Update the first found entry (assuming 'de' or 'en' fields are unique)
+          await strapi.entityService.update('api::controlled-vocabulary.controlled-vocabulary', existingEntries[0].id, { data: item });
+        } else {
+          // Create new entry
+          await strapi.entityService.create('api::controlled-vocabulary.controlled-vocabulary', { data: item });
+        }
+      } catch (error) {
+        console.error('Error importing Controlled Vocabulary translation:', error);
+      }
+    }
+  } else {
+    console.error('File not found:', filePath);
+  }
 }
