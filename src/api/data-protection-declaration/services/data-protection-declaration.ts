025821a83a8d1624971e6e746d3ce8e5b5b8b1d6
mahtabiltarabian <m.iltarabian68@gmail.com>
2024-03-22 22:58:14 +0100

    add data protection declaration

---
+++ src/api/data-protection-declaration/services/data-protection-declaration.ts
@@ -0,0 +1,7 @@
+/**
+ * data-protection-declaration service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::data-protection-declaration.data-protection-declaration');
