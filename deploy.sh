18284ef4f8f3da4dde8455ac5b7a07b2ecb8baa5
Dominic Tölle <dominic.tolle@gmail.com>
2024-06-28 15:32:59 +0200

    WIP

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-UP_TO_DATE=0
-
-[ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
-sed 's/\// /g') | cut -f1) ] && UP_TO_DATE=0 || UP_TO_DATE=1
-
-
-if [ $UP_TO_DATE = 1 ]; then
+    git checkout $1
     git pull
     yarn
     yarn build
-    ./node_modules/.bin/pm2 stop 0
+    ./node_modules/.bin/pm2 stop all
     killall node
     ./node_modules/.bin/pm2 start /usr/home/zoono/zoonotify-cms/ecosystem.config.js
-
-fi
