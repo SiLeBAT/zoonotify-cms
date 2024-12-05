939b90287c81115190e6e76f3d6230f6a42125ca
Dominic Tölle <dominic.tolle@gmail.com>
2024-12-05 14:46:06 +0100

    Update Node Version in pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
         name: 'zoonotify-cms',
         script: 'npm',
         args: 'start',
+        interprester:'node@18.20.5',
         env: {
             NODE_ENV: "production",
           }
