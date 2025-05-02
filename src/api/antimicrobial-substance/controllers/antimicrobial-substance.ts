135821f325a0c7ad8ca895a604c296b5515dd78e
mahtabiltarabian <m.iltarabian68@gmail.com>
2025-05-02 17:21:51 +0200

    committ new data model for resistance whit import script for new releational data and  update import script base on new data model

---
+++ src/api/antimicrobial-substance/controllers/antimicrobial-substance.ts
@@ -0,0 +1,7 @@
+/**
+ * antimicrobial-substance controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::antimicrobial-substance.antimicrobial-substance');
---
+++ src/api/antimicrobial-substance/routes/antimicrobial-substance.ts
@@ -0,0 +1,7 @@
+/**
+ * antimicrobial-substance router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::antimicrobial-substance.antimicrobial-substance');
